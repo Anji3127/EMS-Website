6d60a73c1a576de449183ce703fa13ae47342702
Anji3127 <Anjalishr213@gmail.com>
2025-07-31 23:27:39 +0530

    Fix import path casing for MyContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import MyContext from "../context/MyContext.jsx";
+import MyContext from "./Context/MyContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
